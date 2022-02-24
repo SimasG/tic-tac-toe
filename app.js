3fe7ae09fdfa0f87d0e72fe28bb044a39f27a02a
simutis <54304022+SimasG@users.noreply.github.com>
2022-02-24 19:33:44 +0200

    finished mobile homepage layout

---
+++ app.js
@@ -0,0 +1,18 @@
+const x = document.querySelector(".picker-x");
+const zero = document.querySelector(".picker-zero");
+const xIcon = document.querySelector(".x-icon");
+const zeroIcon = document.querySelector(".zero-icon");
+
+x.addEventListener("click", () => {
+  x.style.backgroundColor = "#a8bfc9";
+  xIcon.style.backgroundImage = "url('../assets/icon-x-transparent.svg')";
+  zero.style.backgroundColor = "#1a2a33";
+  zeroIcon.style.backgroundImage = "url('../assets/icon-o-silver.svg')";
+});
+
+zero.addEventListener("click", () => {
+  zero.style.backgroundColor = "#a8bfc9";
+  zeroIcon.style.backgroundImage = "url('../assets/icon-o-transparent.svg')";
+  x.style.backgroundColor = "#1a2a33";
+  xIcon.style.backgroundImage = "url('../assets/icon-x-silver.svg')";
+});
