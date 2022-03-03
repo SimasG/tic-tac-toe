a1a57fd86144cbcb5b6fead81dc5602e4871c5b8
simutis <54304022+SimasG@users.noreply.github.com>
2022-03-03 16:56:17 +0200

    implemented turn switching and hover states in js

--- js/game.js
+++ js/game.js
@@ -1,39 +1,50 @@
 // GAME -> solo
-const cellElements = document.querySelectorAll("[data-cell]");
-const squareIcons = document.querySelectorAll(".square-icon");
-
 const x_class = "x";
 const circle_class = "circle";
 
-// if true -> o's turn, if false -> x's turn
+const mainGrid = document.getElementById("main-grid");
+const cellElements = document.querySelectorAll("[data-cell]");
+const squareIcons = document.querySelectorAll(".square-icon");
+
+// if true -> o's turn, if false -> x's turn. "let circleTurn" means "circleTurn" is undefined by default.
 let circleTurn;
 
-squareIcons.forEach((squareIcon) => {
-  squareIcon.addEventListener(
-    "click",
-    handleClick,
-    { once: true }
-    // {
-    // squareIcon.style.backgroundImage = "url('../assets/icon-x.svg')";
-    // }
-  );
+cellElements.forEach((cellElement) => {
+  cellElement.addEventListener("click", handleClick, { once: true });
 });
 
 function handleClick(e) {
   cell = e.target;
   // If circleTurn = true, currentClass = circle_class. Else currentClass = x_class.
+  // Since circleTurn is "undefined", currentClass is always "x_class".
   const currentClass = circleTurn ? circle_class : x_class;
-  // squareIcon.style.backgroundImage = "url('../assets/icon-x.svg')";
-  // place mark
+  // PLACE MARK
   placeMark(cell, currentClass);
-  // check for win
-  // check for draw
-  // if no win/draw, switch turns
-  console.log(cell);
+  // CHECK FOR WIN
+  // CHECK FOR DRAW
+  // IF NO WIN/DRAW, SWITCH TURNS
+  swapTurns();
+
+  // SET HOVER STATES
+  setHoverBoardClass();
 }
 
 function placeMark(cell, currentClass) {
   cell.classList.add(currentClass);
+}
+
+function swapTurns() {
+  circleTurn = !circleTurn;
+}
+
+function setHoverBoardClass() {
+  if (circleTurn == true) {
+    mainGrid.classList.add(circle_class);
+    mainGrid.classList.remove(x_class);
+  } else if (circleTurn == false) {
+    mainGrid.classList.add(x_class);
+    mainGrid.classList.remove(circle_class);
+  }
 }
 
 // If x -> active, "x turn" & player 1 starts. If o -> active, "x turn" and CPU starts.
