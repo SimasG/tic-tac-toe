2b5e59c68fba44936e7c9b2b14d72dbc6ed9595e
simutis <54304022+SimasG@users.noreply.github.com>
2022-03-03 17:18:09 +0200

    implemented check for win logic

--- js/game.js
+++ js/game.js
@@ -1,6 +1,16 @@
 // GAME -> solo
 const x_class = "x";
 const circle_class = "circle";
+const winning_combinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 const mainGrid = document.getElementById("main-grid");
 const cellElements = document.querySelectorAll("[data-cell]");
@@ -9,18 +19,29 @@ const squareIcons = document.querySelectorAll(".square-icon");
 // if true -> o's turn, if false -> x's turn. "let circleTurn" means "circleTurn" is undefined by default.
 let circleTurn;
 
-cellElements.forEach((cellElement) => {
-  cellElement.addEventListener("click", handleClick, { once: true });
-});
+startGame();
+
+function startGame() {
+  circleTurn = false;
+  cellElements.forEach((cellElement) => {
+    cellElement.addEventListener("click", handleClick, { once: true });
+  });
+  setHoverBoardClass();
+}
 
 function handleClick(e) {
   cell = e.target;
   // If circleTurn = true, currentClass = circle_class. Else currentClass = x_class.
   // Since circleTurn is "undefined", currentClass is always "x_class".
   const currentClass = circleTurn ? circle_class : x_class;
+
   // PLACE MARK
   placeMark(cell, currentClass);
+
   // CHECK FOR WIN
+  if (checkWin(currentClass)) {
+    console.log("winner");
+  }
   // CHECK FOR DRAW
   // IF NO WIN/DRAW, SWITCH TURNS
   swapTurns();
@@ -47,4 +68,13 @@ function setHoverBoardClass() {
   }
 }
 
-// If x -> active, "x turn" & player 1 starts. If o -> active, "x turn" and CPU starts.
+function checkWin(currentClass) {
+  // .some method returns a boolean value if at least one of the array elements (array in array) meet the criteria
+  return winning_combinations.some((combination) => {
+    // .every method returns "true" if all of the array (combination) elements meet the criteria. "false" otherwise
+    return combination.every((index) => {
+      // check if cell elements with the index numbers in each array contain the same class
+      return cellElements[index].classList.contains(currentClass);
+    });
+  });
+}
